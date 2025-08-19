732983291e980ff9ba2dab1138ff42fc0cc9a6da
flexGURU <mukunajohn329@gmail.com>
2025-08-19 08:19:20 +0300

    feat(router): update noSidebar logic to include account login path

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,58 +1,63 @@
 <template>
-	<FrappeUIProvider>
-		<Layout>
-			<div class="text-base">
-				<router-view />
-			</div>
-		</Layout>
-		<InstallPrompt v-if="isMobile" />
-		<Dialogs />
-	</FrappeUIProvider>
+  <FrappeUIProvider>
+    <Layout>
+      <div class="text-base">
+        <router-view />
+      </div>
+    </Layout>
+    <InstallPrompt v-if="isMobile" />
+    <Dialogs />
+  </FrappeUIProvider>
 </template>
 <script setup>
-import { FrappeUIProvider } from 'frappe-ui'
-import { Dialogs } from '@/utils/dialogs'
-import { computed, onUnmounted, ref, watch } from 'vue'
-import { useScreenSize } from './utils/composables'
-import { usersStore } from '@/stores/user'
-import { useRouter } from 'vue-router'
-import { posthogSettings } from '@/telemetry'
-import DesktopLayout from './components/DesktopLayout.vue'
-import MobileLayout from './components/MobileLayout.vue'
-import NoSidebarLayout from './components/NoSidebarLayout.vue'
-import InstallPrompt from './components/InstallPrompt.vue'
+import { FrappeUIProvider } from "frappe-ui";
+import { Dialogs } from "@/utils/dialogs";
+import { computed, onUnmounted, ref, watch } from "vue";
+import { useScreenSize } from "./utils/composables";
+import { usersStore } from "@/stores/user";
+import { useRouter } from "vue-router";
+import { posthogSettings } from "@/telemetry";
+import DesktopLayout from "./components/DesktopLayout.vue";
+import MobileLayout from "./components/MobileLayout.vue";
+import NoSidebarLayout from "./components/NoSidebarLayout.vue";
+import InstallPrompt from "./components/InstallPrompt.vue";
 
-const { isMobile } = useScreenSize()
-const router = useRouter()
-const noSidebar = ref(false)
-const { userResource } = usersStore()
+const { isMobile } = useScreenSize();
+const router = useRouter();
+const noSidebar = ref(false);
+const { userResource } = usersStore();
 
 router.beforeEach((to, from, next) => {
-	if (to.query.fromLesson || to.path === '/persona' || to.path === '/vmmsportalSignup') {
-		noSidebar.value = true
-	} else {
-		noSidebar.value = false
-	}
-	next()
-})
+  if (
+    to.query.fromLesson ||
+    to.path === "/persona" ||
+    to.path === "/vmmsportalSignup" ||
+    to.path === "/account/login"
+  ) {
+    noSidebar.value = true;
+  } else {
+    noSidebar.value = false;
+  }
+  next();
+});
 
 const Layout = computed(() => {
-	if (noSidebar.value) {
-		return NoSidebarLayout
-	}
-	if (isMobile.value) {
-		return MobileLayout
-	}
-	return DesktopLayout
-})
+  if (noSidebar.value) {
+    return NoSidebarLayout;
+  }
+  if (isMobile.value) {
+    return MobileLayout;
+  }
+  return DesktopLayout;
+});
 
 onUnmounted(() => {
-	noSidebar.value = false
-})
+  noSidebar.value = false;
+});
 
 watch(userResource, () => {
-	if (userResource.data) {
-		posthogSettings.reload()
-	}
-})
+  if (userResource.data) {
+    posthogSettings.reload();
+  }
+});
 </script>
