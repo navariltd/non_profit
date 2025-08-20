b49680d8bbed85fbf3aad9b3e9675e7217ab8aa0
flexGURU <mukunajohn329@gmail.com>
2025-08-20 11:39:05 +0300

    feat(api): add endpoints for retrieving regions and branches

--- non_profit/non_profit/api.py
+++ non_profit/non_profit/api.py
@@ -7,3 +7,13 @@ def get_membership_types():
     return frappe.get_all(
         "Membership Type", fields=["name", "membership_type", "amount"]
     )
+
+
+@frappe.whitelist(allow_guest=True)
+def get_regions():
+    return frappe.get_all("Company", filters={"is_group": 0})
+
+
+@frappe.whitelist(allow_guest=True)
+def get_branches():
+    return frappe.get_all("Branch")
