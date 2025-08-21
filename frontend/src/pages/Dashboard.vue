03d3ce6ac372b6c97fe0ccdb783a99822073656f
flexGURU <mukunajohn329@gmail.com>
2025-08-21 16:40:00 +0300

    feat(Dashboard): implement Member and Volunteer components in Dashboard template

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,3 +1,14 @@
 <template>
-    hi
+  <Member v-if="roleResource.data.non_profit_member" />
+
+  <Volunteer v-if="roleResource.data.employee" />
 </template>
+
+<script lang="ts" setup>
+import Member from "../components/Member.vue";
+import Volunteer from "../components/Volunteer.vue";
+import { usersStore } from "../stores/user";
+import { ref } from "vue";
+
+const { roleResource } = usersStore();
+</script>
