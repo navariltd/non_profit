b994078016bc8f5c70f1d8fc8b9f67d22d6281f9
flexGURU <mukunajohn329@gmail.com>
2025-09-09 12:01:02 +0300

    feat(SignUp): enhance user and membership creation logic with error handling and role assignment

--- non_profit/non_profit/user.py
+++ non_profit/non_profit/user.py
@@ -7,16 +7,12 @@ from frappe.utils.password import update_password
 @frappe.whitelist(allow_guest=True)
 def sign_up(**kwargs):
 
-    print(kwargs)
-
     frappe.db.begin()
 
     try:
 
         if kwargs.get("category_member"):
             user = create_user(kwargs)
-
-            create_member(kwargs, user)
 
         if kwargs.get("category_volunteer"):
             create_volunteer(kwargs)
@@ -29,79 +25,95 @@ def sign_up(**kwargs):
         frappe.throw(str(e))
 
 
-def create_user(kwargs):
+@frappe.whitelist(allow_guest=True)
+def create_user(**kwargs):
 
-    if frappe.db.exists("User", {"email": kwargs.get("email")}):
-        frappe.throw("User already exists with this email")
+    try:
+        if frappe.db.exists("User", {"email": kwargs.get("email")}):
+            frappe.throw("User already exists with this email")
 
-    if frappe.db.get_creation_count("User", 60) > 300:
-        return frappe.respond_as_web_page(
-            _("Temporarily Disabled"),
-            _(
-                "Too many users signed up recently, so the registration is disabled. Please try back in an hour"
-            ),
-            http_status_code=429,
-        )
+        if frappe.db.get_creation_count("User", 60) > 300:
+            return frappe.respond_as_web_page(
+                _("Temporarily Disabled"),
+                _(
+                    "Too many users signed up recently, so the registration is disabled. Please try back in an hour"
+                ),
+                http_status_code=429,
+            )
 
-    user = frappe.get_doc(
-        {
-            "doctype": "User",
-            "email": kwargs.get("email"),
-            "first_name": kwargs.get("first_name"),
-            "last_name": kwargs.get("last_name"),
-            "gender": kwargs.get("gender"),
-            "enabled": 1,
-            "role_profile_name": (
-                "Member" if kwargs.get("category_member") else "Volunteer"
-            ),
-            "module_profile": (
-                "Member" if kwargs.get("category_member") else "Volunteer"
-            ),
-            "default_app": "lms",
-        }
-    )
-
-    user.insert(ignore_permissions=True)
-    update_password(user.name, kwargs.get("password"))
-
-    return user
-
-
-def create_member(kwargs, user=None):
-
-    member = frappe.get_doc(
-        {
-            "doctype": "Member",
-            "member_name": f'{kwargs.get("first_name")} {kwargs.get("last_name")}',
-            "email_id": user.name,
-            "membership_type": kwargs.get("membership_type"),
-            "custom_company": kwargs.get("region"),
-            "custom_branch": kwargs.get("branch"),
-            "pan_number": kwargs.get("phone_number"),
-        }
-    )
-    member.insert(ignore_permissions=True)
-
-    if kwargs.get("membership_type"):
-        doc_name = kwargs.get("membership_type")
-        from_date = datetime.now().date()
-        to_date = add_to_date(from_date, years=1)
-
-        membership = frappe.get_doc(
+        user = frappe.get_doc(
             {
-                "doctype": "Membership",
-                "member": member.name,
-                "membership_type": doc_name,
-                "company": kwargs.get("region"),
-                "branch": kwargs.get("branch"),
-                "membership_status": "Pending",
-                "from_date": from_date,
-                "to_date": to_date,
-                "member_since_date": from_date,
+                "doctype": "User",
+                "email": kwargs.get("email"),
+                "first_name": kwargs.get("first_name"),
+                "last_name": kwargs.get("last_name"),
+                "full_name": f'{kwargs.get("first_name")} {kwargs.get("last_name")}',
+                "phone": kwargs.get("phone"),
+                "gender": kwargs.get("gender"),
+                "enabled": 1,
+                "default_app": "lms",
             }
         )
 
-        membership.insert(ignore_permissions=True)
+        user.insert(ignore_permissions=True)
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Error signing up")
+        frappe.throw("Error signing up")
+
+
+@frappe.whitelist(allow_guest=True)
+def create_membership(**kwargs):
+    try:
+        frappe.db.begin()
+
+        if frappe.db.exists("Member", {"email_id": kwargs.get("email_id")}):
+            frappe.throw("Member already exists with this email")
+
+        member = frappe.get_doc(
+            {
+                "doctype": "Member",
+                "member_name": kwargs.get("member_name"),
+                "email_id": kwargs.get("email_id"),
+                "membership_type": kwargs.get("membership_type"),
+                "custom_company": kwargs.get("region"),
+                "custom_branch": kwargs.get("branch"),
+                "pan_number": kwargs.get("phone_number"),
+            }
+        )
+        member.insert(ignore_permissions=True)
+
+        user = frappe.get_doc("User", {"email": member.email_id})
+        user.role_profile_name = "Member"
+        user.module_profile = "Member"
+
+        user.save(ignore_permissions=True)
+
+        if kwargs.get("membership_type"):
+            doc_name = kwargs.get("membership_type")
+            from_date = datetime.now().date()
+            to_date = add_to_date(from_date, years=1)
+
+            membership = frappe.get_doc(
+                {
+                    "doctype": "Membership",
+                    "member": member.name,
+                    "membership_type": doc_name,
+                    "company": kwargs.get("region"),
+                    "branch": kwargs.get("branch"),
+                    "membership_status": "Pending",
+                    "from_date": from_date,
+                    "to_date": to_date,
+                    "member_since_date": from_date,
+                }
+            )
+
+            membership.insert(ignore_permissions=True)
+
+        frappe.db.commit()
+    except Exception as e:
+        frappe.db.rollback()
+        frappe.log_error(frappe.get_traceback(), "Error creating membership")
+        frappe.throw("Error creating membership")
 
 
 def create_volunteer(kwargs):
