ec04266dfc71f5de1a868f39ade20949c18ecc1a
flexGURU <mukunajohn329@gmail.com>
2025-09-10 16:42:19 +0300

    feat(Dashboard): enhance event display and add confirmation status handling

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -18,33 +18,40 @@
   </div>
 
   <hr />
-  <!-- <div class="flex flex-col gap-2 p-5">
-    <h1 class="text-xl m-2">Upcoming Events</h1>
+  <div
+    class="flex flex-col gap-2 md:p-8 md:w-3/4 mx-auto"
+    v-if="
+      (roleResource.data && roleResource.data.is_volunteer) ||
+      (roleResource.data && roleResource.data.non_profit_member)
+    "
+  >
+    <div class="flex justify-between items-center m-2">
+      <h1 class="text-xl m-2">Upcoming Events</h1>
+      <router-link :to="{ name: 'Events' }">
+        <Button variant="subtle" size="lg" theme="blue">
+          View All Events
+        </Button></router-link
+      >
+    </div>
 
     <div
       v-if="events.data && events.data.length > 0"
-      v-for="event in events.data.filter((e) => e.status === 'Open')"
-      class="p-8 grid grid-cols-1 lg:grid-cols-3 gap-4"
+      class="p-2 grid grid-cols-1 lg:grid-cols-3 gap-4"
     >
-      <EventCard :event="event" />
+      <EventCard v-for="event in events.data.slice(0, 3)" :event="event" />
     </div>
 
     <EmptyState
       v-if="events.data && events.data.length === 0"
       :type="'Events'"
     />
-    <router-link :to="{ name: 'Events' }">
-      <Button variant="subtle" size="lg" theme="green">
-        View All Events
-      </Button>
-    </router-link>
-  </div> -->
+  </div>
 </template>
 <script lang="ts" setup>
-import { inject, onMounted } from "vue";
+import { inject, onMounted, provide, toRaw, watch } from "vue";
 import EmptyState from "../components/EmptyState.vue";
-import EventCard from "../components/EventCard.vue";
-import Member from "../components/Member.vue";
+import EventCard, { Event } from "../components/EventCard.vue";
+import Member from "../components/MemberPlan.vue";
 import Volunteer from "../components/Volunteer.vue";
 import { membershipStore } from "../stores/membership";
 import { usersStore } from "../stores/user";
@@ -57,8 +64,8 @@ const { events, currentMembership } = membershipStore();
 const user = inject<any>("$user");
 
 onMounted(() => {
+  confirmEventStatus.data;
   const { roleResource } = usersStore();
-  console.log("role data", roleResource.data);
 
   if (!user.data) {
     toast.warning("You must be logged in to view this page.");
@@ -69,4 +76,22 @@ onMounted(() => {
     roleResource.reload();
   }
 });
+
+const confirmEventStatus = createResource({
+  url: "non_profit.non_profit.api.confirm_event_status",
+  auto: true,
+  onSuccess(data) {
+    if (events.data) {
+      events.data = events.data.map((e) => {
+        const match = data.find((d: any) => d.event.name === e.name);
+        return {
+          ...e,
+          confirmStatus: match ? match.confirmed : false,
+        };
+      });
+    }
+  },
+});
+
+provide("reloadConfirmStatus", () => confirmEventStatus.reload());
 </script>
