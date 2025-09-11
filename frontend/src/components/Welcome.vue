125f1257899ef9e7e7af88d40d2e99e1e5b132be
flexGURU <mukunajohn329@gmail.com>
2025-09-11 12:08:38 +0300

    refactor(Welcome): remove welcome message and unused props for cleaner component

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="">
     <div class="flex flex-col justify-center h-screen space-y-4 p-12">
-      <h1 class="text-3xl font-bold">Welcome {{ name }}</h1>
       <p class="text-3xl text-gray-600">
         Join us and make a difference. Sign up to become a volunteer or a
         member.
@@ -27,14 +26,6 @@
 <script lang="ts" setup>
 import { Button } from "frappe-ui";
 import router from "../router";
-
-defineProps({
-  name: {
-    type: String,
-    required: false,
-    default: "User",
-  },
-});
 
 function navigateTo(path: string) {
   router.push("/" + path);
