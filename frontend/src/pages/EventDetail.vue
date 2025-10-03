8d7e9205c89391feff92cbf8a040c42ab094bfd2
flexGURU <mukunajohn329@gmail.com>
2025-10-03 18:58:20 +0300

    fix(EventDetail.vue): correct error handling and improve layout for event details display

--- frontend/src/pages/EventDetail.vue
+++ frontend/src/pages/EventDetail.vue
@@ -2,209 +2,213 @@
   <div class="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
     <ProgressSpinner v-if="eventDetail.loading" />
     <ErrorMessage
-      v-if="eventDetail.error"
+      v-else-if="eventDetail.error"
       class="text-center border rounded-md p-2 border-red-500 bg-red-50 text-sm my-auto mt-20"
       message="Failed to load Event Details"
     />
-    <div class="relative overflow-hidden bg-white">
-      <div
-        class="absolute inset-0 bg-gradient-to-r from-red-300 to-transparent"
-      ></div>
-      <div
-        class="max-w-7xl mx-auto px-4 sm:px-2 lg:px-8 py-8 lg:py-10 relative flex flex-col md:flex-row items-center gap-4 md:gap-10"
-      >
-        <div class="flex-1">
-          <h1
-            class="text-3xl sm:text-4xl md:text-5xl lg:text-[3.5rem] font-bold text-gray-800 mb-4 leading-tight"
-          >
-            {{ eventDetail.data?.title }}
-          </h1>
+    <div v-else-if="eventDetail.data">
+      <div class="relative overflow-hidden bg-white">
+        <div
+          class="absolute inset-0 bg-gradient-to-r from-red-300 to-transparent"
+        ></div>
+        <div
+          class="max-w-7xl mx-auto px-4 sm:px-2 lg:px-8 py-8 lg:py-10 relative flex flex-col md:flex-row items-center gap-4 md:gap-10"
+        >
+          <div class="flex-1">
+            <h1
+              class="text-3xl sm:text-4xl md:text-5xl lg:text-[3.5rem] font-bold text-gray-800 mb-4 leading-tight"
+            >
+              {{ eventDetail.data?.title }}
+            </h1>
 
-          <div class="flex flex-wrap gap-4 sm:gap-6 text-gray-700 mb-4 sm:mb-6">
-            <div class="flex items-center gap-2 text-sm sm:text-base">
-              <CalendarDays class="w-5 h-5 text-red-500" />
-              <span
-                >{{ formatDate(eventDetail.data?.start_date) }} -
-                {{ formatDate(eventDetail.data?.end_date) }}</span
+            <div
+              class="flex flex-wrap gap-4 sm:gap-6 text-gray-700 mb-4 sm:mb-6"
+            >
+              <div class="flex items-center gap-2 text-sm sm:text-base">
+                <CalendarDays class="w-5 h-5 text-red-500" />
+                <span
+                  >{{ formatDate(eventDetail.data?.start_date) }} -
+                  {{ formatDate(eventDetail.data?.end_date) }}</span
+                >
+              </div>
+              <div class="flex items-center gap-2 text-sm sm:text-base">
+                <Clock class="w-5 h-5 text-red-500" />
+                <span>{{ eventDetail.data?.start_time }}</span>
+              </div>
+              <div class="flex items-center gap-2 text-sm sm:text-base">
+                <MapPin class="w-5 h-5 text-red-500" />
+                <span>{{ eventDetail.data?.venue }}</span>
+              </div>
+            </div>
+
+            <div class="mb-6 sm:mb-8">
+              <Button
+                theme="red"
+                variant="solid"
+                size="lg"
+                @click="handleRegister"
               >
+                REGISTER NOW
+              </Button>
             </div>
-            <div class="flex items-center gap-2 text-sm sm:text-base">
-              <Clock class="w-5 h-5 text-red-500" />
-              <span>{{ eventDetail.data?.start_time }}</span>
-            </div>
-            <div class="flex items-center gap-2 text-sm sm:text-base">
-              <MapPin class="w-5 h-5 text-red-500" />
-              <span>{{ eventDetail.data?.venue }}</span>
-            </div>
-          </div>
 
-          <div class="mb-6 sm:mb-8">
-            <Button
-              theme="red"
-              variant="solid"
-              size="lg"
-              @click="handleRegister"
-            >
-              REGISTER NOW
-            </Button>
-          </div>
-
-          <div class="grid grid-cols-2 sm:grid-cols-4 gap-3">
-            <div
-              v-for="(value, key) in timeRemaining"
-              :key="key"
-              class="bg-red-50 border border-red-100 rounded-lg py-3 px-1 sm:py-4 text-center"
-            >
-              <div class="text-xl sm:text-3xl font-bold text-gray-800">
-                {{ value }}
-              </div>
-              <div class="text-xs sm:text-sm text-gray-600 uppercase mt-1">
-                {{ key }}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="">
-          <img
-            :src="eventDetail.data?.banner_image"
-            alt="Event Image"
-            class="w-full rounded-lg"
-          />
-        </div>
-      </div>
-    </div>
-
-    <div class="max-w-7xl mx-auto md:px-4 px-1 lg:px-8 py-6 lg:py-8">
-      <div
-        class="bg-white border border-gray-200 rounded-2xl p-6 sm:p-12 shadow-sm"
-      >
-        <h2 class="text-xl md:text-3xl font-extrabold text-red-500 mb-3">
-          Why attend?
-        </h2>
-        <p class="text-base sm:text-lg text-gray-700 leading-relaxed mb-6">
-          {{ eventDetail.data?.short_description }}
-        </p>
-        <hr class="my-4" />
-        <p class="text-base sm:text-lg text-gray-700 leading-relaxed mb-6">
-          {{ eventDetail.data?.about }}
-        </p>
-      </div>
-    </div>
-
-    <div class="max-w-7xl mx-auto md:px-4 px-2 lg:px-8 py-4 lg:py-8">
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <div
-          class="md:col-span-2 bg-white rounded-xl p-6 border border-gray-200 shadow-sm"
-        >
-          <div class="mb-6 flex items-center gap-3">
-            <div
-              class="w-10 h-10 flex items-center justify-center rounded-lg bg-red-50 border border-red-100"
-            >
-              <Users class="w-5 h-5 text-red-500" />
-            </div>
-            <h3 class="text-xl md:text-3xl font-extrabold text-gray-900">
-              Featured Speakers
-            </h3>
-          </div>
-
-          <ul class="space-y-4">
-            <li
-              v-for="speaker in speakerProfiles.data"
-              :key="speaker.name"
-              class="flex flex-col sm:flex-row sm:items-center justify-between gap-4 bg-gray-50 border border-gray-200 rounded-lg p-4"
-            >
-              <div class="flex items-center gap-4">
-                <img
-                  :src="speaker.display_image"
-                  alt="Speaker Image"
-                  class="w-16 h-16 sm:w-20 sm:h-20 rounded-full object-cover border border-gray-200"
-                />
-                <div>
-                  <h4 class="text-lg font-semibold text-gray-800">
-                    {{ speaker.full_name }}
-                  </h4>
-                  <p class="text-sm text-gray-600">
-                    {{ speaker.designation }}
-                  </p>
-                  <p class="text-red-500 font-bold" v-if="speaker.company">
-                    {{ speaker.company }}
-                  </p>
+            <div class="grid grid-cols-2 sm:grid-cols-4 gap-3">
+              <div
+                v-for="(value, key) in timeRemaining"
+                :key="key"
+                class="bg-red-50 border border-red-100 rounded-lg py-3 px-1 sm:py-4 text-center"
+              >
+                <div class="text-xl sm:text-3xl font-bold text-gray-800">
+                  {{ value }}
+                </div>
+                <div class="text-xs sm:text-sm text-gray-600 uppercase mt-1">
+                  {{ key }}
                 </div>
               </div>
+            </div>
+          </div>
 
-              <div>
-                <span class="">Social Links:</span>
-                <div class="flex items-center gap-3 sm:justify-end">
-                  <a
-                    v-for="link in speaker.social_media_links"
-                    :key="link.name"
-                    :href="link.url"
-                    target="_blank"
-                    class="text-gray-500 hover:text-red-500 transition"
-                  >
-                    <component
-                      :is="getSocialMediaIcon(link.platform)"
-                      class="w-5 h-5"
-                    />
-                  </a>
-                </div>
-              </div>
-            </li>
-          </ul>
+          <div class="">
+            <img
+              :src="eventDetail.data?.banner_image"
+              alt="Event Image"
+              class="w-full rounded-lg"
+            />
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="max-w-7xl mx-auto px-1 lg:px-8 pb-8 lg:pb-12">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div class="max-w-7xl mx-auto md:px-4 px-1 lg:px-8 py-6 lg:py-8">
         <div
-          class="bg-white border border-gray-200 rounded-2xl p-6 sm:p-8 shadow-sm"
+          class="bg-white border border-gray-200 rounded-2xl p-6 sm:p-12 shadow-sm"
         >
-          <h3
-            class="text-xl md:text-2xl font-bold text-gray-800 mb-6 border-b border-gray-200 pb-3"
+          <h2 class="text-xl md:text-3xl font-extrabold text-red-500 mb-3">
+            Why attend?
+          </h2>
+          <p class="text-base sm:text-lg text-gray-700 leading-relaxed mb-6">
+            {{ eventDetail.data?.short_description }}
+          </p>
+          <hr class="my-4" />
+          <p class="text-base sm:text-lg text-gray-700 leading-relaxed mb-6">
+            {{ eventDetail.data?.about }}
+          </p>
+        </div>
+      </div>
+
+      <div class="max-w-7xl mx-auto md:px-4 px-2 lg:px-8 py-4 lg:py-8">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div
+            class="md:col-span-2 bg-white rounded-xl p-6 border border-gray-200 shadow-sm"
           >
-            Event Details
-          </h3>
-
-          <div class="space-y-4 divide-y divide-gray-100">
-            <div
-              class="flex items-start gap-3 pt-1 first:pt-0"
-              v-for="detail in [
-                {
-                  icon: CalendarDays,
-                  label: 'Date',
-                  value:
-                    formatDate(eventDetail.data?.start_date) +
-                    ' - ' +
-                    formatDate(eventDetail.data?.end_date),
-                },
-                {
-                  icon: Clock,
-                  label: 'Time',
-                  value:
-                    eventDetail.data?.start_time +
-                    ' - ' +
-                    eventDetail.data?.end_time,
-                },
-                {
-                  icon: MapPin,
-                  label: 'Venue',
-                  value: eventDetail.data?.venue,
-                },
-                { icon: Users, label: 'Host', value: eventDetail.data?.host },
-              ]"
-              :key="detail.label"
-            >
+            <div class="mb-6 flex items-center gap-3">
               <div
                 class="w-10 h-10 flex items-center justify-center rounded-lg bg-red-50 border border-red-100"
               >
-                <component :is="detail.icon" class="w-5 h-5 text-red-500" />
+                <Users class="w-5 h-5 text-red-500" />
               </div>
-              <div>
-                <div class="text-sm text-gray-500">{{ detail.label }}</div>
-                <div class="text-base font-medium text-gray-800">
-                  {{ detail.value }}
+              <h3 class="text-xl md:text-3xl font-extrabold text-gray-900">
+                Featured Speakers
+              </h3>
+            </div>
+
+            <ul class="space-y-4">
+              <li
+                v-for="speaker in speakerProfiles.data"
+                :key="speaker.name"
+                class="flex flex-col sm:flex-row sm:items-center justify-between gap-4 bg-gray-50 border border-gray-200 rounded-lg p-4"
+              >
+                <div class="flex items-center gap-4">
+                  <img
+                    :src="speaker.display_image"
+                    alt="Speaker Image"
+                    class="w-16 h-16 sm:w-20 sm:h-20 rounded-full object-cover border border-gray-200"
+                  />
+                  <div>
+                    <h4 class="text-lg font-semibold text-gray-800">
+                      {{ speaker.full_name }}
+                    </h4>
+                    <p class="text-sm text-gray-600">
+                      {{ speaker.designation }}
+                    </p>
+                    <p class="text-red-500 font-bold" v-if="speaker.company">
+                      {{ speaker.company }}
+                    </p>
+                  </div>
+                </div>
+
+                <div>
+                  <span class="">Social Links:</span>
+                  <div class="flex items-center gap-3 sm:justify-end">
+                    <a
+                      v-for="link in speaker.social_media_links"
+                      :key="link.name"
+                      :href="link.url"
+                      target="_blank"
+                      class="text-gray-500 hover:text-red-500 transition"
+                    >
+                      <component
+                        :is="getSocialMediaIcon(link.platform)"
+                        class="w-5 h-5"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="max-w-7xl mx-auto px-1 lg:px-8 pb-8 lg:pb-12">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div
+            class="bg-white border border-gray-200 rounded-2xl p-6 sm:p-8 shadow-sm"
+          >
+            <h3
+              class="text-xl md:text-2xl font-bold text-gray-800 mb-6 border-b border-gray-200 pb-3"
+            >
+              Event Details
+            </h3>
+
+            <div class="space-y-4 divide-y divide-gray-100">
+              <div
+                class="flex items-start gap-3 pt-1 first:pt-0"
+                v-for="detail in [
+                  {
+                    icon: CalendarDays,
+                    label: 'Date',
+                    value:
+                      formatDate(eventDetail.data?.start_date) +
+                      ' - ' +
+                      formatDate(eventDetail.data?.end_date),
+                  },
+                  {
+                    icon: Clock,
+                    label: 'Time',
+                    value:
+                      eventDetail.data?.start_time +
+                      ' - ' +
+                      eventDetail.data?.end_time,
+                  },
+                  {
+                    icon: MapPin,
+                    label: 'Venue',
+                    value: eventDetail.data?.venue,
+                  },
+                  { icon: Users, label: 'Host', value: eventDetail.data?.host },
+                ]"
+                :key="detail.label"
+              >
+                <div
+                  class="w-10 h-10 flex items-center justify-center rounded-lg bg-red-50 border border-red-100"
+                >
+                  <component :is="detail.icon" class="w-5 h-5 text-red-500" />
+                </div>
+                <div>
+                  <div class="text-sm text-gray-500">{{ detail.label }}</div>
+                  <div class="text-base font-medium text-gray-800">
+                    {{ detail.value }}
+                  </div>
                 </div>
               </div>
             </div>
@@ -213,12 +217,17 @@
       </div>
     </div>
   </div>
-  <AttendEventModal v-model="isOpen" />
+  <AttendEventModal
+    :eventId="eventDetail.data.name"
+    :dialogStatus="isOpen"
+    @close="isOpen = false"
+    v-model="isOpen"
+  />
 </template>
 <script setup>
 import { Button, createResource, toast } from "frappe-ui";
 import { CalendarDays, Clock, MapPin, Users } from "lucide-vue-next";
-import { ref, onMounted, onUnmounted, inject } from "vue";
+import { ref, onMounted, onUnmounted, inject, watch } from "vue";
 import { useRoute } from "vue-router";
 import ProgressSpinner from "../components/Common/ProgressSpinner.vue";
 import ErrorMessage from "frappe-ui/src/components/ErrorMessage/ErrorMessage.vue";
@@ -258,7 +267,7 @@ const eventDetail = createResource({
         toast.error(
           "This is a private event. Please log in  to view event details."
         );
-      }, 2000);
+      }, 3000);
       router.push({ name: "Login" });
     }
     speakerProfiles.reload();
