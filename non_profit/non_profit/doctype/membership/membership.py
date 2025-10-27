6537bb751e8fb2b559e99672588866570826541a
flexGURU <mukunajohn329@gmail.com>
2025-10-27 18:16:29 +0300

    fix(Membership): change membership status from Active to Pending during creation

--- non_profit/non_profit/doctype/membership/membership.py
+++ non_profit/non_profit/doctype/membership/membership.py
@@ -79,7 +79,7 @@ class Membership(Document):
         end_date = get_cycle_dates(start_date, membership_type.billing_cycle, cycles)
 
         self.to_date = end_date
-        self.membership_status = "Active"
+        self.membership_status = "Pending"
 
     def create_member_from_website_user(self):
         member_name = frappe.get_value("Member", dict(email_id=frappe.session.user))
