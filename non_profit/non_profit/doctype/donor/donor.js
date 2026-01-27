d5e89a9d01c27565337427aeeafa8f808eb200c8
muruthigitau <muruthigitau@gmail.com>
2026-01-27 11:36:24 +0300

    feat(donor): enhance Donor with customer creation and UI improvements
    Add server-side logic and client UI to create and link a Customer
    from a Donor for accounting purposes.
    
    Key changes:
    - Implemented customer creation from Donor, with optional
      auto-create on insert based on settings.
    - Ensure "Beneficiary" customer group exists before creating customers.
    - Store linked Customer reference on the Donor record.
    - Added contact fields to Donor (email, phone, salutation, image).
    - Reorganized Donor form layout with improved sections and tabs.
    - Added "Create Customer" button with conditional visibility.
    - Improved address and contact rendering and clearing logic.

--- non_profit/non_profit/doctype/donor/donor.js
+++ non_profit/non_profit/doctype/donor/donor.js
@@ -1,17 +1,32 @@
 // Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Donor', {
-	refresh: function(frm) {
-		frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Donor'};
+frappe.ui.form.on("Donor", {
+  refresh: function (frm) {
+    frappe.dynamic_link = { doc: frm.doc, fieldname: "name", doctype: "Donor" };
 
-		frm.toggle_display(['address_html','contact_html'], !frm.doc.__islocal);
+    frm.toggle_display(["address_html", "contact_html"], !frm.doc.__islocal);
+    frm.toggle_display(
+      "create_customer",
+      !frm.doc.customer && !frm.doc.__islocal,
+    );
 
-		if(!frm.doc.__islocal) {
-			frappe.contacts.render_address_and_contact(frm);
-		} else {
-			frappe.contacts.clear_address_and_contact(frm);
-		}
+    if (!frm.doc.__islocal) {
+      frappe.contacts.render_address_and_contact(frm);
+    } else {
+      frappe.contacts.clear_address_and_contact(frm);
+    }
+  },
 
-	}
+  create_customer: (frm) => {
+    frm.call({
+      method: "create_customer",
+      doc: frm.doc,
+      callback: function (r) {
+        if (r.message) {
+          frappe.msgprint("Customer " + r.message + " created.");
+        }
+      },
+    });
+  },
 });
